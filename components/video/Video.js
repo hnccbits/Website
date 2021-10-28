2bea892f5f9bf686bd2bb859a81d54151a497b89
Zeeshan-2k1 <ashraf2k1zeeshan@gmail.com>
2021-10-28 22:19:18 +0530

    Fixed #14 Bug in Video Play on Header Section of Home Page

--- components/video/Video.js
+++ components/video/Video.js
@@ -12,38 +12,39 @@ const Video = () => {
     document.getElementById('videoContainer').style.opacity = 0;
     document.getElementById('video').pause();
   };
+
+  const Heading = ({ src, text }) => {
+    return (
+      <h1
+        onMouseEnter={() => VideoPlay(src)}
+        onMouseOver={() => VideoPlay(src)}
+        onMouseOut={VideoPause}
+        onTouchStart={() => VideoPlay(src)}
+      >
+        {text}
+        <span />
+      </h1>
+    );
+  };
+
   return (
     <section className={Styles.video}>
       <div id="videoContainer" className={Styles.designVideoContainer}>
-        <video src={video} autoPlay loop controls={false} id="video">
+        <video
+          src={video}
+          autoPlay
+          muted="muted"
+          loop
+          controls={false}
+          id="video"
+        >
           <track kind="captions" />
         </video>
       </div>
       <div className={Styles.content}>
-        <h1
-          onMouseOver={() => VideoPlay('/video/design.mp4')}
-          onMouseOut={VideoPause}
-          onTouchStart={() => VideoPlay('/video/design.mp4')}
-        >
-          Design.
-          <span />
-        </h1>
-        <h1
-          onMouseOver={() => VideoPlay('/video/develop.mp4')}
-          onMouseOut={VideoPause}
-          onTouchStart={() => VideoPlay('/video/develop.mp4')}
-        >
-          Develop.
-          <span />
-        </h1>
-        <h1
-          onMouseOver={() => VideoPlay('/video/code.mp4')}
-          onMouseOut={VideoPause}
-          onTouchStart={() => VideoPlay('/video/code.mp4')}
-        >
-          Code.
-          <span />
-        </h1>
+        <Heading src="/video/design.mp4" text="Design." />
+        <Heading src="/video/develop.mp4" text="Develop." />
+        <Heading src="/video/code.mp4" text="Code." />
       </div>
       <div className={Styles.mobileVideo}>
         <video controls={false} autoPlay src="/video/develop.mp4">
