4d69204236650f2b1acbff31d46984c3bdec4fb0
Arm Supakorn <arm.supakorn@bitkub.com>
2021-10-29 23:30:27 +0700

    feat: add social icon

--- components/teams/TeamCard.js
+++ components/teams/TeamCard.js
@@ -1,5 +1,11 @@
 import Image from 'next/image';
 import Style from './Team.module.css';
+import {
+  FaFacebookSquare,
+  FaLinkedin,
+  FaInstagram,
+  FaGithubSquare,
+} from 'react-icons/fa';
 
 const TeamCard = ({ name, title, socials, imageSrc }) => {
   return (
@@ -18,7 +24,40 @@ const TeamCard = ({ name, title, socials, imageSrc }) => {
         <h3 className="text-xl">{name}</h3>
         <h4>{title}</h4>
       </div>
-      <div>{socials}</div>
+      <div className="z-10 text-left w-full">
+        {socials?.fb && (
+          <div className="flex mt-1">
+            <FaFacebookSquare size={24} />
+            <a href={socials?.fb} className="ml-2">
+              {socials?.fb}
+            </a>
+          </div>
+        )}
+        {socials?.linkedin && (
+          <div className="flex mt-1">
+            <FaLinkedin size={24} />
+            <a href={socials?.linkedin} className="ml-2">
+              {socials?.linkedin}
+            </a>
+          </div>
+        )}
+        {socials?.insta && (
+          <div className="flex mt-1">
+            <FaInstagram size={24} />
+            <a href={socials?.insta} className="ml-2">
+              {socials?.insta}
+            </a>
+          </div>
+        )}
+        {socials?.gb && (
+          <div className="flex mt-1">
+            <FaGithubSquare size={24} />
+            <a href={socials?.gb} className="ml-2">
+              {socials?.gb}
+            </a>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
