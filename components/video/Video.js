aa624932a782e5073e0b75b635af3ac6478a65d3
Zeeshan-2k1 <ashraf2k1zeeshan@gmail.com>
2021-10-30 14:40:47 +0530

    Optimized the video play in background

--- components/video/Video.js
+++ components/video/Video.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import Styles from './Video.module.css';
 
 const Video = () => {
-  const [video, setVideo] = useState('/video/test.mp4');
+  const [video, setVideo] = useState('design');
   const VideoPlay = (src) => {
     setVideo(src);
     document.getElementById('videoContainer').style.opacity = 1;
@@ -11,6 +11,7 @@ const Video = () => {
   const VideoPause = () => {
     document.getElementById('videoContainer').style.opacity = 0;
     document.getElementById('video').pause();
+    setVideo('');
   };
 
   const Heading = ({ src, text }) => {
@@ -29,23 +30,57 @@ const Video = () => {
     <section className={Styles.video}>
       <div id="videoContainer" className={Styles.designVideoContainer}>
         <video
-          src={video}
+          src="/video/design.mp4"
           autoPlay
           muted="muted"
           loop
           controls={false}
           id="video"
+          className={video === 'design' ? 'top-0 left-0 z-40' : 'z-0 -top-full'}
+          style={{
+            width: video === 'design' ? '100vw' : '0vw',
+          }}
+        >
+          <track kind="captions" />
+        </video>
+        <video
+          src="/video/develop.mp4"
+          autoPlay
+          muted="muted"
+          loop
+          controls={false}
+          id="video"
+          className={
+            video === 'develop' ? 'top-0 left-0 z-40' : 'z-0 -top-full'
+          }
+          style={{
+            width: video === 'develop' ? '100vw' : '0vw',
+          }}
+        >
+          <track kind="captions" />
+        </video>
+        <video
+          src="/video/code.mp4"
+          autoPlay
+          muted="muted"
+          loop
+          controls={false}
+          id="video"
+          className={video === 'code' ? 'top-0 left-0 z-40' : 'z-0 -top-full'}
+          style={{
+            width: video === 'code' ? '100vw' : '0vw',
+          }}
         >
           <track kind="captions" />
         </video>
       </div>
       <div className={Styles.content}>
-        <Heading src="/video/design.mp4" text="Design." />
-        <Heading src="/video/develop.mp4" text="Develop." />
-        <Heading src="/video/code.mp4" text="Code." />
+        <Heading src="design" text="Design." />
+        <Heading src="develop" text="Develop." />
+        <Heading src="code" text="Code." />
       </div>
       <div className={Styles.mobileVideo}>
-        <video controls={false} autoPlay src="/video/develop.mp4">
+        <video controls={false} autoPlay loop src="/video/develop.mp4">
           <track kind="captions" />
         </video>
       </div>
