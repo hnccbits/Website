4bb9cbfbabf3b0e3455f26772568f0f45ae91b31
Vishwajeet Singh <80119038+vishwajeet-hash@users.noreply.github.com>
2022-01-28 22:16:01 +0530

    Teams section updated (#38)

--- pages/teams.js
+++ pages/teams.js
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import Screen from '../components/screen/Screen';
 import TeamCard from '../components/teams/TeamCard';
 import Styles from '../components/teams/Team.module.css';
-import TeamData2k19 from '../lib/data/TeamData';
+import { TeamData2k18, TeamData2k19, TeamData2k20 } from '../lib/data/TeamData';
 import BottomGlitter from '../components/StyledText/BottomGlitter';
 
 function Teams() {
@@ -19,6 +19,20 @@ function Teams() {
             each member is the team.
             <br />- Phil Jackson
           </h3>
+        </div>
+        <h2 className={Styles.postHead}>Board Of Directors</h2>
+        <div className={Styles.cardContainer}>
+          {TeamData2k18.slice(0, 17).map((item, index) => {
+            return (
+              <TeamCard
+                key={`${String(index)}-team`}
+                name={item.name}
+                // title={item.title}
+                imageSrc={item.imageSrc}
+                socials={item.socials}
+              />
+            );
+          })}
         </div>
 
         <h2 className={Styles.postHead}>Post Bearers</h2>
@@ -54,6 +68,21 @@ function Teams() {
         <h2 className={Styles.postHead}>Event Manager</h2>
         <div className={`${Styles.cardContainer}`}>
           {TeamData2k19.slice(15, 20).map((item, index) => {
+            return (
+              <TeamCard
+                key={`${String(index)}-team`}
+                name={item.name}
+                title={item.title}
+                imageSrc={item.imageSrc}
+                socials={item.socials}
+              />
+            );
+          })}
+        </div>
+
+        <h2 className={Styles.postHead}>Our 2k20 Members</h2>
+        <div className={`${Styles.cardContainer}`}>
+          {TeamData2k20.slice(0, 26).map((item, index) => {
             return (
               <TeamCard
                 key={`${String(index)}-team`}
